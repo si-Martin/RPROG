ea8b3dfc0dff578adf659800d13ac1f3cacbcdb2
si-Martin <si-Martin@users.noreply.github.com>
2014-11-23 17:51:06 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,28 +1,28 @@
 #makeCacheMatrix takes matrix from the argument, calculates the inverse and store 
-#both values to a list, which is accessable globally
+#both values to a list, which can retrieved globally
 
 makeCacheMatrix <- function(matrixARG) {
   matrixV <-  matrixARG   
   matrixI <- solve(matrixV) # calculates matrix inverse
     
   if (exists("Mcache") == FALSE) (Mcache <<- list()) # checks if global variable exists
-  Mcache[[length(Mcache)+1]] <<- matrixV # ads matrix 
-  Mcache[[length(Mcache)+1]] <<- matrixI  # ads matrix inverse
+  Mcache[[length(Mcache)+1]] <<- matrixV # adds matrix 
+  Mcache[[length(Mcache)+1]] <<- matrixI  # adds matrix inverse
 
 }
 
 cacheSolve <- function(matrixARG) {
-#takes user defined matrix from the argument, it check the global variable
+#takes the user defined matrix from the argument, it checks the global variable
 #if the matrix has already calculated inverse matrix. If so, it retrieves the 
-#inverse from the list, else invokes makeCacheMatrix to calculate and store 
-#the inverse and retireves it again from the global variable
+#inverse from the list, else it invokes makeCacheMatrix to calculate and to store 
+#the inverse and retrieves it again from the global variable
   
   R <- matrix()
-  if (exists("Mcache") == FALSE) #check whether the global variable exists, ie. function is run for the first time
+  if (exists("Mcache") == FALSE) #check whether the global variable exists, ie. function has been run for the first time
     {(makeCacheMatrix(matrixARG)) #calls the function  which calculates the inverse
-    R <- Mcache[2] #retrieves the inverse // as it is new, it is placed at second place 
+    R <- Mcache[2] #retrieves the inverse // as it is new, it is placed at a second place 
     }
-  else # if it is not new, some inverted matrixes have been calculated
+  else # if it is not new, some inverted matrices have been calculated
    {for (i in 1:6) #the code runs through the stored values.   
     {if (identical(Mcache[i], matrixARG) == TRUE) #if it finds identical matrix, it takes the inverse
       {R <- Mcache[i+1]        #the inverse is stored after the initial matrix
